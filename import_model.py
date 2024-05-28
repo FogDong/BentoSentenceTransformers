7e5552e5f3fc8c9accff5a5196837e5569f8d0e1
FogDong <fog@bentoml.com>
2024-05-28 10:57:25 +0800

    fix: use cpu
    Signed-off-by: FogDong <fog@bentoml.com>

--- import_model.py
+++ import_model.py
@@ -6,11 +6,11 @@ BENTO_MODEL_TAG = MODEL_ID.lower().replace("/", "--")
 
 def import_model(model_id, bento_model_tag):
 
-    import torch
+    # import torch
     from sentence_transformers import SentenceTransformer, models
 
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = SentenceTransformer(MODEL_ID, device=device)
+    # device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = SentenceTransformer(MODEL_ID, device="cpu")
 
     with bentoml.models.create(bento_model_tag) as bento_model_ref:
         model.save(bento_model_ref.path)
